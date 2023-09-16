aecbaa5a236b0bb4f0d0ac0b2fe706ce5b7ebf8c
Utkarsh khambra <utkarsh.khamb@gmail.com>
2023-09-17 00:35:01 +0530

    Day1

---
+++ 2021/day1.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <cassert>
+#include <charconv>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <ranges>
+#include <vector>
+int main()
+{
+    using std::operator""sv;
+    std::ifstream input("input/day1");
+    assert(input);
+
+    std::vector<int> nums(std::istream_iterator<int> { input }, std::istream_iterator<int>());
+    int count = std::ranges::count_if(nums | std::views::adjacent<2>, [](auto a) noexcept {
+        // std::cout << std::get<0>(a) << " " << std::get<1>(a) << std::endl;
+        return std::get<0>(a) < std::get<1>(a);
+    });
+
+    std::cout << count << "\n";
+
+    auto f = nums | std::views::slide(3) | std::views::transform([](auto a) noexcept -> int {
+                 int sum = 0;
+                 for (auto i: a)
+                     sum += i;
+                 return sum;
+             });
+
+    count = std::ranges::count_if(f | std::views::adjacent<2>, [](auto a) noexcept {
+        // std::cout << std::get<0>(a) << " " << std::get<1>(a) << std::endl;
+        return std::get<0>(a) < std::get<1>(a);
+    });
+    ;
+    std::cout << count << "\n";
+}
---
+++ 2021/day1.pl
@@ -0,0 +1,48 @@
+use strict;
+use warnings;
+
+open my $input_fh, '<', "input/day1" or die "Cannot open input data file: $!";
+
+sub sum($)
+{
+    my $list = shift;
+    my $sum  = 0;
+    foreach my $i (@$list)
+    {
+        $sum += $i;
+    }
+    return $sum;
+}
+
+my $count = 0;
+my @nums;
+while (my $num = <$input_fh>)
+{
+    chomp $num;
+    push @nums, $num;
+}
+close $input_fh;
+
+foreach my $i (0 .. $#nums - 1)
+{
+    my ($first, $second) = @nums[$i .. $i + 1];
+    $count += $first < $second;
+}
+
+print $count , "\n";
+$count = 0;
+my @nums_3;
+foreach my $i (0 .. $#nums - 2)
+{
+    my @window = @nums[$i .. $i + 2];
+    # print join ',', @window , "\n";
+    push @nums_3, sum(\@window);
+}
+
+foreach my $i (0 .. $#nums_3 - 1)
+{
+    my @window = @nums_3[$i .. $i + 2];
+    $count += $window[0] < $window[1];
+}
+
+print $count;
