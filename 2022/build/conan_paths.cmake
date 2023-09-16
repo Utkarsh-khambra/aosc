33882891b026b88cc4264d6038e954f8dc0821bc
Utkarsh khambra <utkarsh.khamb@gmail.com>
2023-09-16 09:22:08 +0530

    Seperate everything by year

---
+++ 2022/build/conan_paths.cmake
@@ -0,0 +1,3 @@
+set(CONAN_FMT_ROOT "/home/utkarsh/.conan/data/fmt/8.0.1/_/_/package/2c09c8f84c016041549fcee94e4caae5d89424b6")
+set(CMAKE_MODULE_PATH "/home/utkarsh/.conan/data/fmt/8.0.1/_/_/package/2c09c8f84c016041549fcee94e4caae5d89424b6/" ${CONAN_FMT_ROOT} ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
+set(CMAKE_PREFIX_PATH "/home/utkarsh/.conan/data/fmt/8.0.1/_/_/package/2c09c8f84c016041549fcee94e4caae5d89424b6/" ${CONAN_FMT_ROOT} ${CMAKE_PREFIX_PATH} ${CMAKE_CURRENT_LIST_DIR})
